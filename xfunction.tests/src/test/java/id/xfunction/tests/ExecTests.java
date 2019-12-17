c60004039d64b73b2497ba738ec9e29b952dfa08
lambdaprime <id.blackmesa@gmail.com>
2019-12-16 20:09:13 -0800

    Adding Exec class

---
+++ xfunction.tests/src/test/java/id/xfunction/tests/ExecTests.java
@@ -0,0 +1,36 @@
+package id.xfunction.tests;
+
+import static java.util.stream.Collectors.toList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import id.xfunction.Exec;
+import id.xfunction.Exec.Result;
+
+public class ExecTests {
+
+    @Test
+    public void test_run() throws Exception {
+        Result result = new Exec("echo", "hello").run();
+        var out = result.stdout.collect(toList());
+        var err = result.stderr.collect(toList());
+        assertEquals(1, out.size());
+        assertEquals(0, err.size());
+        assertEquals(0, result.code.get().intValue());
+        assertEquals("hello", out.get(0));
+    }
+
+    @Test
+    public void test_run_with_error() throws Exception {
+        Result result = new Exec("ls /asdff").run();
+        var out = result.stdout.collect(toList());
+        var err = result.stderr.collect(toList());
+        assertEquals(0, out.size());
+        assertEquals(1, err.size());
+        assertEquals(2, result.code.get().intValue());
+        assertTrue(err.get(0).toUpperCase().contains("NO SUCH FILE"));
+    }
+
+}
---
+++ xfunction/src/main/java/id/xfunction/Exec.java
@@ -0,0 +1,86 @@
+package id.xfunction;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.concurrent.Future;
+import java.util.stream.Stream;
+
+/**
+ * Run shell command and obtain its output as a stream of lines.
+ */
+public class Exec {
+
+    private String[] cmd;
+    private boolean singleLine;
+    private Stream<String> input;
+
+    /**
+     * Holds result of command execution
+     */
+    public static class Result {
+        public Stream<String> stdout;
+        public Stream<String> stderr;
+        public Future<Integer> code;
+        Result(Stream<String> stdout, Stream<String> stderr) { this.stdout = stdout; this.stderr = stderr; }
+    }
+
+    /**
+     * Constructor which accepts the command to run.
+     * First item of the array should be the command itself and the
+     * rest items are arguments of it.  
+     */
+    public Exec(String... cmd) {
+        this.cmd = cmd;
+    }
+
+    /**
+     * Constructor which accepts the full command line to run
+     */
+    public Exec(String cmd) {
+        this(new String[] {cmd});
+        singleLine = true;
+    }
+
+    /**
+     * Given input will be sent to command's stdin.
+     */
+    public Exec withInput(Stream<String> input) {
+        this.input = input;
+        return this;
+    }
+
+    /**
+     * Run the command and send it the input if any
+     */
+    public Result run() {
+        try {
+            Process p = runProcess();
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(p.getInputStream()));
+            if (input != null) {
+                PrintStream ps = new PrintStream(p.getOutputStream(), true);
+                input.forEach(ps::println);
+                ps.close();
+            }
+            BufferedReader ein = new BufferedReader(
+                new InputStreamReader(p.getErrorStream()));
+            Result result = new Result(in.lines(), ein.lines());
+            result.code = p.onExit().thenApply(proc -> proc.exitValue());
+            return result;
+            
+        } catch (Exception e) {
+            throw new RuntimeException("Encountered error executing command: " + Arrays.toString(cmd), e);
+        }
+    }
+
+    private Process runProcess() throws IOException {
+        if (singleLine) {
+            return Runtime.getRuntime().exec(cmd[0]);
+        }
+        return new ProcessBuilder(cmd).start();
+    }
+
+}
