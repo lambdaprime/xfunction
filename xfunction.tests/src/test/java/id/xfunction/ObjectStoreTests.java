30286ebcc4b483089da77137a896af587a9949ba
lambdaprime <id.blackmesa@gmail.com>
2020-03-29 11:28:55 -0700

    Let users decide how they want to store objects in ObjectsStore - in a Set or not (in case they want to store some singleton object)

---
+++ xfunction.tests/src/test/java/id/xfunction/ObjectStoreTests.java
@@ -0,0 +1,32 @@
+package id.xfunction;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashSet;
+
+import org.junit.jupiter.api.Test;
+
+public class ObjectStoreTests {
+
+    @Test
+    public void test_happy() throws Exception {
+        String obj = "enity2";
+        Path file = Paths.get("/tmp/store" + System.currentTimeMillis());
+        ObjectStore<String> store = new ObjectStore<>(file);
+        store.save(obj);
+        assertEquals(obj, store.load().get());
+    }
+
+    @Test
+    public void test_happy_collection() throws Exception {
+        HashSet<String> obj = new HashSet<>();
+        Path file = Paths.get("/tmp/store" + System.currentTimeMillis());
+        ObjectStore<HashSet<String>> store = new ObjectStore<>(file);
+        obj.add("enity1");
+        obj.add("enity2");
+        store.save(obj);
+        assertEquals(obj, store.load().get());
+    }
+}
